91809902fb33cb605658e92d109526bec5e3130c
Tobias Fromm <t.fromm@jacobs-university.de>
2018-03-06 13:43:06 +0100

    added example template for experiments

---
+++ scripts/pr2_experiment_template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import rospy
+
+if __name__ == '__main__':
+    rospy.sleep(10.)
+    print(' >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(' >>> running PR2 experiment template script')
+    print(' >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    rospy.sleep(10.)
