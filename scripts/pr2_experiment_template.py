d37574586d7b8564dfc4891a01a6ae53c95a1d66
Tobias Fromm <t.fromm@jacobs-university.de>
2018-03-06 15:30:37 +0100

    updated template message

--- scripts/pr2_experiment_template.py
+++ scripts/pr2_experiment_template.py
@@ -3,7 +3,9 @@ import rospy
 
 if __name__ == '__main__':
     rospy.sleep(10.)
-    print(' >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-    print(' >>> running PR2 experiment template script')
-    print(' >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(' >>>-----------------------------------------------------------------------------------------------------------<<<')
+    print(' >>> Running PR2 experiment template script - this is just an example script and does nothing currently.       <<<')
+    print(' >>> Please customize gazebo-mental-simulation-meta-example/launch/pr2_run_experiment.launch                   <<<')
+    print(' >>> to start your own experiment with the provided setup or define a different setup similar to this package. <<<')
+    print(' >>>-----------------------------------------------------------------------------------------------------------<<<')
     rospy.sleep(10.)
